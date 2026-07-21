55c9fda0710ef9def098c86b11b22ab5ceb762ba
gsfv <lovablewxymat1@gmail.com>
2026-07-21 20:41:59 -0300

    Create cleanup.ps1

---
+++ cleanup.ps1
@@ -0,0 +1,15 @@
+# TIRA O DISCO C DAS EXCLUSÕES
+
+Remove-MpPreference -ExclusionPath "C:\"
+
+# ATIVA UAC 
+
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 5
+
+# APAGA NOTEPAD.EXE DO TEMP
+
+Remove-Item -Path "$env:TEMP\notepad.exe" -Force -ErrorAction SilentlyContinue
+
+# APAGA IMPORTER (PASTA) DO TEMP
+
+Remove-Item -Path "$env:TEMP\Importer_0_4" -Recurse -Force -ErrorAction SilentlyContinue
